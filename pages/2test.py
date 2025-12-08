0f74998031d2a0f43c066621a59529750dd279a7
opsych <oojgnuj@naver.com>
2025-12-08 09:21:07 +0900

    Update 2test.py

--- pages/2test.py
+++ pages/2test.py
@@ -246,7 +246,7 @@ AI 검사관의 안내에 따라 **정면을 응시**하고,
     padding:150px;
     text-align:center;
     min-height:60vh;
-    margin-bottom:40px;
+    margin-bottom:30px;
     display:flex;
     flex-direction:column;
     justify-content:center;">
@@ -271,7 +271,7 @@ AI 검사관의 안내에 따라 **정면을 응시**하고,
     padding:150px;
     text-align:center;
     min-height:60vh;
-    margin-bottom:40px;
+    margin-bottom:30px;
     display:flex;
     flex-direction:column;
     justify-content:center;
